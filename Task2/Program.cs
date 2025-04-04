5968605839a11da56679ac0be529a9f029eb91c3
Dmytro Chumak <chumakde@gmail.com>
2025-04-04 12:24:35 +0200

    First commit

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Task2
+﻿using System;
+
+namespace Task2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
         }
     }
 }
